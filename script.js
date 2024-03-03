2a916b9c327b79784008c051e8e93d13aa5c6245
CHIGODI <chiegody254@gmail.com>
2024-03-03 11:13:59 +0300

    Add js logic for adding book

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 const myLibrary = [];
 
+/**
+ * Book - defines Book class
+ */
 class Book {
-    constructor(title) {
+    constructor(title, author, pages, read) {
         this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
-};
-
-function addBookToLibrary(book) {
-   myLibrary.push(book); 
 }
 
-for (let i = 0; i < myLibrary.length; i++) {
-    console.log(myLibrary[i]);
+/**
+ * addBook - this function creates an instance of book
+ */
+function addBook() {
+    const titleInput = document.getElementById('title');
+    const authorInput = document.getElementById('author');
+    const pagesInput = document.getElementById('pages');
+    const readCheckbox = document.getElementById('read');
+
+    const bookTitle = titleInput.value.trim();
+    const bookAuthor = authorInput.value.trim();
+    const bookPages = parseInt(pagesInput.value, 10);
+    const read = readCheckbox.checked;
+    
+    const newBook = new Book(bookTitle, bookAuthor, bookPages, read);
+    addBookToLibrary(newBook);
+
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    readCheckbox.checked = false;
+    displayLibrary();
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayLibrary() {
+    console.log('Library:', myLibrary);
 }
