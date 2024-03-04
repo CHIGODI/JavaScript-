38fa853381d5b0a059b768a64fd3c4645838e0dc
CHIGODI <chiegody254@gmail.com>
2024-03-04 17:23:49 +0300

    Add js to render book if submit is clicked

--- Libary_app/script.js
+++ Libary_app/script.js
@@ -12,6 +12,23 @@ class Book {
     }
 }
 
+function displayLibrary() {
+    console.log(myLibrary)
+    for(let i = 0; i < myLibrary.length; i++){
+        const libraryContainer = document.getElementById('library-container');
+        console.log(libraryContainer);
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('library-book');
+        bookDiv.innerHTML = `<p>Title: ${myLibrary[i].title}</p>
+                            <p>Author: ${myLibrary[i].author}</p>
+                            <p>Pages: ${myLibrary[i].pages}</p>
+                            <p>Read: ${myLibrary[i].read ? 'Yes' : 'No'}</p>`;
+    
+        libraryContainer.appendChild(bookDiv);
+    }
+    
+}
+
 /**
  * addBook - this function creates an instance of book
  */
@@ -26,20 +43,27 @@ function addBook() {
     const bookPages = parseInt(pagesInput.value, 10);
     const read = readCheckbox.checked;
     
-    const newBook = new Book(bookTitle, bookAuthor, bookPages, read);
-    addBookToLibrary(newBook);
-
-    titleInput.value = '';
-    authorInput.value = '';
-    pagesInput.value = '';
-    readCheckbox.checked = false;
-    displayLibrary();
+    if (bookTitle !== '' && bookAuthor !== '' && !isNaN(bookPages)){
+        const newBook = new Book(bookTitle, bookAuthor, bookPages, read);
+        addBookToLibrary(newBook);
+    
+        titleInput.value = '';
+        authorInput.value = '';
+        pagesInput.value = '';
+        readCheckbox.checked = false;
+        displayLibrary();
+    }
 }
-
-function addBookToLibrary(book) {
+function  addBookToLibrary(book){
     myLibrary.push(book);
 }
 
-function displayLibrary() {
-    console.log('Library:', myLibrary);
+function showForm() {
+    const formContainer = document.querySelector('.container');
+    const currentDisplay = getComputedStyle(formContainer).display;
+    if (currentDisplay === 'none') {
+        formContainer.style.display = 'flex';
+    } else {
+        formContainer.style.display = 'none';
+    }
 }
